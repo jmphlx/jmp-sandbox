90d70ea7d59c49ea1547fb0ab8134ca6fa803030
Laurel Timko <laurel.timko@jmp.com>
2026-02-24 11:11:36 -0500

    Update logging

--- scripts/update-configs.mjs
+++ scripts/update-configs.mjs
@@ -3,7 +3,6 @@ import fs from 'node:fs/promises';
 export default async function sendPostRequest(authToken, yamlText, configType) {
   const url = `https://admin.hlx.page/config/jmphlx/sites/jmp-sandbox/content/${configType}`;
 
-  console.log(url);
   try {
     const response = await fetch(url, {
       method: 'POST',
@@ -15,13 +14,9 @@ export default async function sendPostRequest(authToken, yamlText, configType) {
       body: yamlText,
     });
     console.log(response);
-
     if (!response.ok) return null;
-    console.log(response);
-    const json = await response.json();
-    console.log(json);
-    return json;
 
+    return await response.json();
   } catch (error) {
     console.error('post request: ', { error });
   }
