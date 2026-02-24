5ae84b79a8e020fa4841c82a1d7f57b88c7ca423
Laurel Timko <laurel.timko@jmp.com>
2026-02-24 10:47:25 -0500

    Add github action for updating indices

---
+++ scripts/update-configs.mjs
@@ -0,0 +1,37 @@
+import fs from 'node:fs/promises';
+
+export default async function sendPostRequest(authToken, yamlText, configType) {
+  const url = `https://admin.hlx.page/config/jmphlx/sites/jmp-sandbox/content/${configType}`;
+
+  console.log(url);
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': `token ${authToken}`,
+        'Accept': '*/*',
+        'Content-Type': 'text/yaml',
+      }, 
+      body: yamlText,
+    });
+    console.log(response);
+
+    if (!response.ok) return null;
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+    return json;
+
+  } catch (error) {
+    console.error('post request: ', { error });
+  }
+}
+
+const authToken = process.env.AUTH_TOKEN;
+const configPath = process.env.CONFIG_PATH;
+const configName = process.env.CONFIG_NAME;
+
+const yamlText = await fs.readFile(configPath, 'utf8');
+
+const result = await sendPostRequest(authToken, yamlText, configName);
+console.log(result);
