e1021f71994b2720d3b34c433bcb9f9d8d02cd02
Laurel Timko <laurel.timko@jmp.com>
2026-05-26 16:16:30 -0400

    Add logging to update config step

--- scripts/update-configs.mjs
+++ scripts/update-configs.mjs
@@ -21,6 +21,8 @@ export default async function sendPostRequest(authToken, yamlText, configType) {
       body: yamlText,
     });
 
+    console.log(response);
+
     if (!response.ok) return null;
     return response;
   } catch (error) {
